dca6a40fd755a55f2f325bda4ccfd38722663334
KelStore <107879165+KelStore@users.noreply.github.com>
2022-07-01 20:35:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let chalk = require('chalk')
 global.owner = [
   ['62895330379186'],
   ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['62895330379186', 'Randi', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@Randi.Bot'
+  var sticker_author = 'Bot'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '© Randi'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
